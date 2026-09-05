e81c637c2a213b6f3b04446886682b873af13767
RBurcia <burciaroberto9@gmail.com>
2026-09-05 14:40:42 -0700

    Made some changes to how it returns and revisited how the normal search works

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -8,7 +8,7 @@ vector<int> twoSum(const vector<int>& nums, int target){
     unordered_map<int, int> index;
 
     for(int i = 0; i < nums.size(); ++i){
-        int needed = target-nums[i];
+        int needed = target-nums[i];// 24-15 = 9 which is at index 14
 
         if(index.count(needed)){
             return{index[needed], i};
@@ -19,8 +19,8 @@ vector<int> twoSum(const vector<int>& nums, int target){
 }
 
 int main(){
-    vector<int> nums = {3,2,4};
-    int target = 6;
+    vector<int> nums = {15, 4, 18, 8, 19, 22, 24, 59, 59, 20, 18, 12, 36, 42, 9};
+    int target = 24;
 
     vector<int> answer = twoSum(nums, target);
 
--- Assignment1BF.cpp
+++ Assignment1BF.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 // make your vector function called twosum
-vector<int> twoSumBF(const vector<int>& array, int target){
+    vector<int> twoSumBF(const vector<int>& array, int target){
     //Creates my 'neededs' for each index before searching through array
+    vector<vector<int>> answers;
     for(int i = 0; i < array.size(); ++i){
-        int needed = target - array[i];
+        int needed = target - array[i]; // for example 24-15 = 
         //Search through array 
-        for(int j = i+1; j < array.size(); ++j){
+        for(int j = 0; j < array.size(); ++j){
             if(array[j] == needed){
                 return {i,j};
             }
@@ -21,8 +22,8 @@ vector<int> twoSumBF(const vector<int>& array, int target){
 
 int main(){
 
-    vector<int> nums = {3,2,4};
-    int target = 6;
+    vector<int> nums = {15, 4, 18, 8, 19, 22, 24, 59, 59, 20, 18, 12, 36, 42, 9};
+    int target = 24;
 
     vector<int> answer = twoSumBF(nums, target);
 
@@ -32,6 +33,4 @@ int main(){
     else{
         cout << "No solution\n";
     }
-
-    return 0;
 }
