fcf7e628a5dae4edcfef2cf981bbf76ac16f844a
RBurcia <burciaroberto9@gmail.com>
2026-09-13 21:07:52 -0700

    Finalized testing and fix errors

--- Assignment2/Itterative_BS.cpp
+++ Assignment2/Itterative_BS.cpp
@@ -13,11 +13,13 @@ int Itterative_BS(const int* array, int high, int target, int& compare_I){
 
         // checks right side
         if(array[mid] < target){
+            compare_I++;
             low = mid + 1;
         }
 
         //checks left side
         else if(array[mid] > target){
+            compare_I++;
             high = mid - 1;
         }
 
@@ -29,6 +31,7 @@ int Itterative_BS(const int* array, int high, int target, int& compare_I){
         //add on to the number of comparisons
         compare_I++;
     }
+    return -1;
 }
 
 int Recursive_BS(const int* array, int low, int high, int target, int& compare_R){
@@ -57,11 +60,9 @@ int Recursive_BS(const int* array, int low, int high, int target, int& compare_R
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(){
-    int comparisons = 0;
     int low = 0;
-    int array[] = { 2, 4, 7, 10, 11, 32, 45, 87 };
-    int key2 = 0;
-    int high = sizeof(array) / sizeof(array[0]);
+    int array[] = { 2, 4, 7, 9, 10, 11, 32, 45, 87 };
+    int high = (sizeof(array) / sizeof(array[0])) - 1;
 
     // Test to be made:
     //first element **
@@ -71,38 +72,95 @@ int main(){
     //missing value outside of range
 
 /////////////////////////////Test for itterative/////////////////////////////
-//&&&&&&&&&&&&&&  Testing first element   &&&&&&&&&&&&&&
-    int key_first = 2;
+    cout << "Starting testing for Iterative Binary Search" << endl << endl;
 
-    int Test_first_I = Itterative_BS(array, high, key_first, comparisons);
-    cout << "Key at Index: " << Test_first_I << " and required " << comparisons << " comparisons" << endl;
+//&&&&&&&&&&&&&&  Testing first element   &&&&&&&&&&&&&&
+    int comparisons = 0;
+    int key_first = 2;
+    int Test_First_I = Itterative_BS(array, high, key_first, comparisons);
+
+    cout << "key_first at Index: " << Test_First_I << " and required " << comparisons << " comparisons" << endl;
 
 //&&&&&&&&&&&&&&  Testing last element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key_last = 87;
+    int Test_Last_I = Itterative_BS(array, high, key_last, comparisons);
 
-    int Test_Last_I = Itterative_BS(array, high, key2, comparisons);
-
-    cout << "Key at Index: " << Test_Last_I << " and required " << comparisons << " comparisons" << endl;
+    cout << "key_last at Index: " << Test_Last_I << " and required " << comparisons << " comparisons" << endl;
 
 //&&&&&&&&&&&&&&  Testing middle element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key_middle = 10;
+    int Test_Middle_I = Itterative_BS(array, high, key_middle, comparisons);
 
+    cout << "key_middle at Index: " << Test_Middle_I << " and required " << comparisons << " comparisons" << endl;
 
 
 //&&&&&&&&&&&&&&  Testing missing element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key_missing = 1;
+    int Test_Missing_I = Itterative_BS(array, high, key_missing, comparisons);
+
+    cout << "key_missing at Index: " << Test_Missing_I << " and required " << comparisons << " comparisons" << endl;
 
 
 
 //&&&&&&&&&&&&&&  Testing missing value outside of range   &&&&&&&&&&&&&&
 //Hint: we can just copy lat element testing and plug a high that is not the max length
+    comparisons = 0;
+    int high_not = (sizeof(array) / sizeof(array[0]))-2;
+    int key_hidden = 87;
+    int Test_Hidden_I = Itterative_BS(array, high_not, key_hidden, comparisons);
+
+    cout << "key_hidden at Index: " << Test_Hidden_I << " and required " << comparisons << " comparisons" << endl << endl;
 
 
 
 
 
 /////////////////////////////Test for recursive/////////////////////////////
-    cout << "Testing for recursive" << endl;
+    cout << "Starting testing for Recurcsive Binary Search" << endl << endl;
 
-    int Test_R = Recursive_BS(array, low, high, key_first, comparisons);
-    cout << "Key for recurssion at Index: " << Test_R << " and required " << comparisons << " comparisons" << endl;
+//&&&&&&&&&&&&&&  Testing first element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key2_first = 2;
+    int Test2_First_I = Recursive_BS(array, low, high, key2_first, comparisons);
+
+    cout << "key_first at Index: " << Test2_First_I << " and required " << comparisons << " comparisons" << endl;
+
+//&&&&&&&&&&&&&&  Testing last element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key2_last = 87;
+    int Test2_Last_I = Recursive_BS(array, low, high, key2_last, comparisons);
+
+    cout << "key_last at Index: " << Test2_Last_I << " and required " << comparisons << " comparisons" << endl;
+
+//&&&&&&&&&&&&&&  Testing middle element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key2_middle = 10;
+    int Test2_Middle_I = Recursive_BS(array, low, high, key2_middle, comparisons);
+
+    cout << "key_middle at Index: " << Test2_Middle_I << " and required " << comparisons << " comparisons" << endl;
+
+
+//&&&&&&&&&&&&&&  Testing missing element   &&&&&&&&&&&&&&
+    comparisons = 0;
+    int key2_missing = 1;
+    int Test2_Missing_I = Recursive_BS(array, low, high, key2_missing, comparisons);
+
+    cout << "key_missing at Index: " << Test2_Missing_I << " and required " << comparisons << " comparisons" << endl;
+
+
+
+//&&&&&&&&&&&&&&  Testing missing value outside of range   &&&&&&&&&&&&&&
+//Hint: we can just copy lat element testing and plug a high that is not the max length
+    comparisons = 0;
+    int high2_not = (sizeof(array) / sizeof(array[0]))-2;
+    int key2_hidden = 87;
+    int Test2_Hidden_I = Recursive_BS(array, low, high2_not, key2_hidden, comparisons);
+
+    cout << "key_hidden at Index: " << Test2_Hidden_I << " and required " << comparisons << " comparisons" << endl;
+
     
 
     return 0;
